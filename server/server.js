85ee4884f16312171270dcfd67f32bd0279b5343
Romain Goutte-Fangeas <rom.gouttefangeas@gmail.com>
2018-11-16 12:06:18 +0100

    Fix server port conflict

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(helmet());
 // api routes
 routes(router);
 
-const port = process.env.NODE_ENV === 'production' ? 80 : 5000;
+const port = 4000;
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
