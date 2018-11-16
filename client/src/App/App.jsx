47434d8b6e5c1d12652e4055cc23b29c612065d3
Victor <victorduvercors@gmail.com>
2018-11-16 15:33:36 +0100

    less init

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { history } from '../_helpers';
 import { alertActions } from '../_actions';
-import { PrivateRoute } from '../_components';
-import { HomePage } from '../HomePage';
 import { LoginPage } from '../LoginPage';
 import { RegisterPage } from '../RegisterPage';
-import {ProfilePage} from "../ProfilePage";
-import {CommunityPage} from "../CommunityPage";
-import {LeaderboardPage} from "../LeaderboardPage";
 import {HeaderComponent} from "../HeaderComponent";
+import {Route} from "react-router-dom";
 
 class App extends React.Component {
     constructor(props) {
@@ -27,17 +22,15 @@ class App extends React.Component {
     render() {
         const { alert } = this.props;
         return (
-                <div className="container">
-                    <div className="col-sm-8 col-sm-offset-2">
-                        {alert.message &&
-                            <div className={`alert ${alert.type}`}>{alert.message}</div>
-                        }
-                            <div>
-                                <Route path="/" component={HeaderComponent}/>
-                                <Route path="/login" component={LoginPage} />
-                                <Route path="/register" component={RegisterPage} />
-                            </div>
-                    </div>
+            <div className="page-container">
+                {alert.message &&
+                <div className={`alert ${alert.type}`}>{alert.message}</div>
+                }
+                <div>
+                    <Route path="/" component={HeaderComponent}/>
+                    <Route path="/login" component={LoginPage} />
+                    <Route path="/register" component={RegisterPage} />
+                </div>
             </div>
         );
     }
