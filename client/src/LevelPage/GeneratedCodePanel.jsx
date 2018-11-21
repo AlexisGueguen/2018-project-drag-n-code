2dd56257aa37c59be24155c12de5305545119dbb
Romain Goutte-Fangeas <rom.gouttefangeas@gmail.com>
2018-11-21 15:55:56 +0100

    Initialize level page

---
+++ client/src/LevelPage/GeneratedCodePanel.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class GeneratedCodePanel extends React.Component {
+    render() {
+        return (
+            <div className="generated-code">Generated code</div>
+        );
+    }
+}
---
+++ client/src/LevelPage/SubmissionPanel.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class SubmissionPanel extends React.Component {
+    render() {
+        return (
+            <div className="submission">Submission</div>
+        );
+    }
+}
