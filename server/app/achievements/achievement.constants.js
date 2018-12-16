206e1a8476a70af5dd4e09e424aba5a7e2313953
Victor <victorduvercors@gmail.com>
2018-12-16 18:47:22 +0100

    update achievements ids

--- server/app/achievements/achievement.constants.js
+++ server/app/achievements/achievement.constants.js
@@ -1,10 +1,10 @@
 module.exports = {
     LevelFinished: '5c14082136a03b55e0c7bd38',
     Rank1: '5c150507e82c2555241b9c27',
-    Score100: '5c1505d3e82c2555241b9c2a', // TODO
+    Score100: '5c1682829d5a146ec425f68b', // TODO
     Score500: '5c1505d3e82c2555241b9c2a',
-    DifficultyMedium: '5c1505d3e82c2555241b9c2a',  // TODO
-    DifficultyHard: '5c1505d3e82c2555241b9c2a',  //TODO
+    DifficultyMedium: '5c16825a9d5a146ec425f68a',  // TODO
+    DifficultyHard: '5c16823c9d5a146ec425f689',  //TODO
     HomeLevels: '5c15074de82c2555241b9c2e',
     LevelLiked: '5c150691e82c2555241b9c2c',
     Level100Likes: '5c150667e82c2555241b9c2b',
