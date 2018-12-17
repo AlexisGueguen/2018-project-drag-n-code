de333f6a879a5d3139e0279d3755c5515687a073
Romain Goutte-Fangeas <rom.gouttefangeas@gmail.com>
2018-12-17 18:14:19 +0100

    update db scripts

--- scripts/levels.js
+++ scripts/levels.js
@@ -25,7 +25,7 @@ db.levels.insertMany([
         "difficulty": 1
     },
     {
-        "title": "Miror",
+        "title": "No difference",
         "description": "Given an array of number in input, you will print this array.",
         "statement": "Given an array of number in input, you will print this array.\ For example, if the input is [1, 2, 3], then the output must be [1, 2, 3]",
         "inputs": "1\n2\n3",
@@ -34,10 +34,10 @@ db.levels.insertMany([
         "author": "",
         "isCreatedByCommunity": false,
         "upVotes": 0,
-        "difficulty": 1
+        "difficulty": 2
     },
     {
-        "title": "",
+        "title": "Bagpack",
         "description": "Given a set of items, each with a weight and a value, determine the number of each item to include in a collection so that the total weight is less than or equal to a given limit and the total value is as large as possible.",
         "statement": "Given a set of items, each with a weight and a value, determine the number of each item to include in a collection so that the total weight is less than or equal to a given limit and the total value is as large as possible.\nA 1998 study of the Stony Brook University Algorithm Repository showed that, out of 75 algorithmic problems, the knapsack problem was the 19th most popular and the third most needed after suffix trees and the bin packing problem.\nKnapsack problems appear in real-world decision-making processes in a wide variety of fields, such as finding the least wasteful way to cut raw materials, selection of investments and portfolios, selection of assets for asset-backed securitization, and generating keys for the Merkle–Hellman and other knapsack cryptosystems.",
         "inputs": "1\n2\n3\n5\n3\n6\n3\n2\n7",
